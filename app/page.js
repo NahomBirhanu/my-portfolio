191e69cc05f2424e949dd6f0598f93b1d6b379ef
your-correct-username <your-correct-email@example.com>
2025-02-14 01:41:09 +0300

    services component done

--- app/page.js
+++ app/page.js
@@ -2,12 +2,14 @@
 import About from "./componenets/About";
 import Header from "./componenets/Header";
 import Navbar from "./componenets/Navbar";
+import Services from "./componenets/Services";
 export default function Home() {
   return (
    <>
    <Navbar/>
    <Header/>
    <About/>
+   <Services/>
 
    </>
   );
