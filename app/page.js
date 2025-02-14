b7e03a921bd2dd01aeb5a09ffd67d708c441446a
your-correct-username <your-correct-email@example.com>
2025-02-14 13:17:44 +0300

    dark mode for navbar

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 'use client'
+import { useEffect, useState } from "react";
 import About from "./componenets/About";
 import Contact from "./componenets/Contact";
 import Footer from "./componenets/Footer";
@@ -7,15 +8,40 @@ import Navbar from "./componenets/Navbar";
 import Services from "./componenets/Services";
 import Work from "./componenets/Work";
 export default function Home() {
+
+
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(()=>{
+    if(localStorage.theme==='dark' || (!('theme' in localStorage) && window.
+    matchMedia('(prefers-color-scheme:dark)').matches)){
+      setIsDarkMode(true)
+
+    }else{
+      setIsDarkMode(false)
+    }
+
+  },[])
+
+  useEffect(()=>{
+    if(isDarkMode){
+      document.documentElement.classList.add('dark');
+      localStorage.theme='dark';
+    }else{
+      document.documentElement.classList.remove('dark');
+      localStorage.theme='';
+    }
+  },[isDarkMode])
+
   return (
    <>
-   <Navbar/>
-   <Header/>
-   <About/>
-   <Services/>
-   <Work/>
-   <Contact/>
-   <Footer/>
+   <Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode}/>
+   <Header isDarkMode={isDarkMode}/>
+   <About isDarkMode={isDarkMode}/>
+   <Services isDarkMode={isDarkMode}/>
+   <Work isDarkMode={isDarkMode}/>
+   <Contact isDarkMode={isDarkMode}/>
+   <Footer isDarkMode={isDarkMode}/>
 
    </>
   );
